426a9b614b5ea16af1d3a96d03038b2198e1bf4d
MartirosAl <145045481+MartirosAl@users.noreply.github.com>
2023-12-07 16:29:53 +0300

    Update Functions.c

--- Functions.c
+++ Functions.c
@@ -7,20 +7,14 @@ unsigned long long Square(const long long value_)
 
 int Reverse(const int number_)
 {
-	int last_digit = 0;
-	int norm_number = number_;
-	int reverse_number = 0;
-	while (norm_number > 0)
+	int n = number_;
+	int rev = 0, rem;
+	while (n > 0)
 	{
-		last_digit = norm_number % 10;
-		reverse_number = reverse_number * 10 + last_digit;
-		norm_number /= 10;
+		rem = n % 10; 
+		rev = rev * 10 + rem; 
+		n = n / 10; 
 	}
-	return reverse_number;
-	// 1234
-	// reverse_number = 0.1;
-	// last_digit = 4
-	// reverse_number = reverse_number * 10 + last_digit
 }
 
 bool IsPalindrome(const int number_)
