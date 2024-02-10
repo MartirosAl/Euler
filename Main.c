5cf5eccc7d6b352439c54b2e8b3b5b304eca4bab
MartirosAl <145045481+MartirosAl@users.noreply.github.com>
2024-02-10 10:31:01 +0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -25,8 +25,22 @@ int main()
 
   /* unsigned int number = 0;
    printf("number = ");
-   scanf_s("%du", &number);*/
+   scanf_s("%du", &number);
    unsigned long long number_ = 600851475143;
-   printf("%u", Problem_5(20));
+   printf("%u", Problem_5(20));*/
+
+   	/*
+	unsigned int number;
+	printf("Enter number:");
+	scanf_s("%u", &number);
+
+	printf("Difference is: %u\n", problem6(number));
+	*/
+
+	unsigned int number;
+	printf("Enter number: ");
+	scanf_s("%u", &number);
+
+	printf("%ust prime is: %u\n", number, Problem_7(number));
    return;
 }
